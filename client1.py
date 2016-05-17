d2a85fb4c351501d83873e1d6ca9e9cf46f6eba8
mon3 <seniut.monika@gmail.com>
2016-05-18 00:27:06 +0200

    dodane nowe, juz pisze wiecej

--- client1.py
+++ client1.py
@@ -1,16 +1,29 @@
 import os
 import socket
 host = "192.168.1.102" # set to IP address of target computer
-port = 13009
+port = 13020
+buf = 1024
 addr = (host, port)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect(addr)
 
+i = 0 
 while True:
-    data = raw_input("Enter message to send or type 'exit': ")
-    sock.sendto(data, addr)
-    if data == "exit":
-        break
+	if (i == 0):
+		data1 = sock.recv(buf)
+		print data1
+		data = raw_input()
+		i += 1
+	elif (i == 1):
+		data1 = sock.recv(buf)
+		print data1
+		data = raw_input()
+		i += 1
+	else:
+		data = raw_input("Enter message to send or type 'exit': ")
+	sock.sendto(data, addr)
+	if data == "exit":
+		break
 sock.close()
 os._exit(0)
--- server1.py
+++ server1.py
@@ -6,7 +6,7 @@ import socket
 
 
 host ="192.168.1.102"
-port = 13009
+port = 13020
 buf = 1024
 addr = (host, port)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,15 +15,22 @@ print "Waiting to receive messages..."
 
 sock.listen(1)
 conn, addr = sock.accept()
-#i = 0
+i = 0
+data = []
 while True:
-	# if (i=0):
-	# 	text1 = "Podaj liczbe liter"
-	# 	sock.sendto(data, addr)
-	#connection, client_address = sock.accept()
-	#i += 1
-	data = conn.recv(buf)
-	print "Received message: " + data
+	if (i == 0):
+		text1 = "Podaj liczbe liter"
+		conn.sendall(text1)
+		i += 1
+	elif (i == 1):
+		text1 = "Wybierz poziom: A, B, C"
+		conn.sendall(text1)
+		i += 1
+	else:
+		
+		data = conn.recv(buf)
+		print "Received message: " + data
+
 	if data == "exit":
 		break
 sock.close()
